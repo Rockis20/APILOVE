709cdfd49334346c719890c45943265553b2f822
Rockis20 <rherrero20056@iesjoanramis.org>
2024-12-05 08:56:28 +0100

    Add files via upload

--- API REST/jsjs/loveCalculator.js
+++ API REST/jsjs/loveCalculator.js
@@ -3,20 +3,20 @@ const data = null;
 const xhr = new XMLHttpRequest();
 xhr.withCredentials = true;
 
-
 xhr.addEventListener('readystatechange', function () {
 	if (this.readyState === this.DONE) {
 		let resultado = JSON.parse(this.response)
 		console.log(this.response);
+	    document.getElementById("%").value = resultado.percentage
 	}
-	document.getElementById("nom1").value = resultado.fname
-	document.getElementById("nom2").value = resultado.fname
-	document.getElementById("%").value = resultado.percentatge
 });
 
+document.getElementById("buto").addEventListener("click", function(){
+	let nom1 = document.getElementById("nom1").value
+	let nom2 = document.getElementById("nom2").value
 
-xhr.open('GET', 'https://love-calculator.p.rapidapi.com/getPercentage?sname='+nom1+'&fname='+nom2');
-xhr.setRequestHeader('X-RapidAPI-Key', '8d5886dc28mshcb8cb4af2de6751p1d614bjsnc8828f0f22b3');
-xhr.setRequestHeader('X-RapidAPI-Host', 'love-calculator.p.rapidapi.com');
-
-xhr.send();
+	xhr.open('GET', 'https://love-calculator.p.rapidapi.com/getPercentage?sname='+nom1+'&fname='+nom2);
+    xhr.setRequestHeader('X-RapidAPI-Key', '8d5886dc28mshcb8cb4af2de6751p1d614bjsnc8828f0f22b3');
+    xhr.setRequestHeader('X-RapidAPI-Host', 'love-calculator.p.rapidapi.com');
+    xhr.send();
+})
